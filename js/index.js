13c948d9fb4d8ade4350c2fd3b465e9d0e813649
ucorpor <ucorpor@gmail.com>
2024-07-21 18:10:06 +0400

    initial logic

--- js/index.js
+++ js/index.js
@@ -1,6 +1,13 @@
-initWatch("startH", "startM", "07:00");
-initWatch("wakeH", "wakeM", "02:00");
-initWatch("wakeH", "wakeM", "00:15");
+initWatch("start-h", "start-m", "07:00");
+initWatch("wake-h", "wake-m", "02:00");
+initWatch("offset-h", "offset-m", "00:15");
+initWatch("day-dream-h-1", "day-dream-m-1", "01:30");
+initWatch("day-dream-h-2", "day-dream-m-2", "00:00");
+initWatch("day-dream-h-3", "day-dream-m-3", "00:00");
+initWatch("day-dream-h-4", "day-dream-m-4", "00:00");
+
+const calcBtn = document.getElementById("calc-btn");
+calcBtn.addEventListener("click", (e) => calc());
 
 function initWatch(hoursId, minsId, time = "00:00") {
   const hours = document.getElementById(hoursId);
@@ -18,19 +25,15 @@ function initWatch(hoursId, minsId, time = "00:00") {
     mins.add(option);
   }
 
-  const timeArray = time.split(":").map((x) => parseInt(x));
-  hours.value = timeArray[0];
-  mins.value = timeArray[1];
+  setTime(hoursId, minsId, timeToMins(time));
 }
 
 function getMins(hoursId, minsId) {
-  const hoursList = document.getElementById(hoursId);
-  const minsList = document.getElementById(minsId);
-
-  const hours = parseInt(hoursList.value);
-  const mins = parseInt(minsList.value);
-  const total = hours * 60 + mins;
-  return total;
+  const hoursList = document.getElementById(hoursId).value;
+  const minsList = document.getElementById(minsId).value;
+  const time = `${hoursList}:${minsList}`;
+  const mins = timeToMins(time);
+  return mins;
 }
 
 function setTime(hoursId, minsId, mins) {
@@ -42,4 +45,49 @@ function setTime(hoursId, minsId, mins) {
 
   hoursList.value = h;
   minsList.value = m;
+}
+
+function timeToMins(time) {
+  const timeArray = time.split(":").map((x) => parseInt(x));
+  const hours = timeArray[0];
+  const mins = timeArray[1];
+  const total = hours * 60 + mins;
+  return total;
+}
+
+function minsToTime(mins) {
+  let h = Math.trunc(mins / 60);
+  const m = mins - h * 60;
+  //while (h > 23) h -= 24;
+  const time = `${String(h).padStart(2, "0")}:${String(m).padStart(2, "0")}`;
+  return time;
+}
+
+function calc() {
+  const start = getMins("start-h", "start-m");
+  const wake = getMins("wake-h", "wake-m");
+  const offset = getMins("offset-h", "offset-m");
+  const dayDreams = [];
+  for (let i = 1; i <= 4; i++) {
+    const dayDreamMins = getMins(`day-dream-h-${i}`, `day-dream-m-${i}`);
+    dayDreams.push(dayDreamMins);
+  }
+
+  let curOffset = 0;
+  let time = start;
+  let result = `${minsToTime(time)} — Подъём\n`;
+  for (let i = 0; i < dayDreams.length; i++) {
+    const dayDream = dayDreams[i];
+    if (dayDream > 0) {
+      const dreamStart = time + wake + curOffset;
+      const dreamEnd = dreamStart + dayDream;
+      result = `${result}${minsToTime(dreamStart)}-${minsToTime(dreamEnd)} — сон №${i + 1}\n`;
+      curOffset += offset;
+      time = dreamEnd;
+    }
+  }
+
+  time = time + wake + curOffset;
+  result = `${result}${minsToTime(time)}  — Отбой\n`;
+  document.getElementById("output").value = result;
 }
